8aa422ff3ee828e631ece9ad5d82896fb2c870df
Deokgyu Yang <secugyu@gmail.com>
2021-05-10 23:14:28 +0900

    Update vendor information to T825N0KOU3CTD1
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I04f226aab273c0f0adbc2c0117ce6d2a730ff269

--- lineage_gts3llte.mk
+++ lineage_gts3llte.mk
@@ -8,21 +8,19 @@ $(call inherit-product, device/samsung/gts3llte/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_gts3llte
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gts3llte
-PRODUCT_BRAND := Samsung
+PRODUCT_NAME := lineage_gts3llte
+PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T825
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
+# Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="gts3llte" \
-    PRODUCT_NAME="gts3llte" \
-    PRIVATE_BUILD_DESC="gts3lltexx-user 9 PPR1.180610.011 T825XXU3CSH7 release-keys"
+    PRODUCT_NAME=gts3llte \
+    PRIVATE_BUILD_DESC="gts3lltekx-user 9 PPR1.180610.011 T825N0KOU3CTD1 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "samsung/gts3lltexx/gts3llte:9/PPR1.180610.011/T825XXU3CSH7:user/release-keys"
-
-TARGET_VENDOR := Samsung
+BUILD_FINGERPRINT := "samsung/gts3lltekx/gts3llte:9/PPR1.180610.011/T825N0KOU3CTD1:user/release-keys"
