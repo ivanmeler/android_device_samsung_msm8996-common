47008580ce7d18b5c6095d795a5636a945aeda63
Deokgyu Yang <secugyu@gmail.com>
2021-05-12 18:10:01 +0900

    Remove seccomp policy
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I836bd7a54dbde378dadfdce55f14bcdbaf2dcb95

--- device.mk
+++ device.mk
@@ -360,11 +360,6 @@ PRODUCT_PACKAGES += \
     libxml2 \
     libprotobuf-cpp-full
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
