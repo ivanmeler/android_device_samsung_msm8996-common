c4c9c2f895e3d6befc8a210d29a698743c8fe013
Deokgyu Yang <secugyu@gmail.com>
2021-05-20 11:18:23 +0900

    gts3l: seccomp: Add mediaextractor.policy
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I56cf60a9f8acf86e9884a94c4ef26c751973ebca

--- device.mk
+++ device.mk
@@ -341,7 +341,8 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
