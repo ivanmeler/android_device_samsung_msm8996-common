7d4800cc3e0388c8edb4243876b93a8eb06ef059
Deokgyu Yang <secugyu@gmail.com>
2021-05-21 14:52:27 +0900

    gts3l: camera: (WIP) Try to fix camera
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I14a6ba62f65c47839bd784cb889edc26e706dbbd

--- device.mk
+++ device.mk
@@ -133,12 +133,13 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera.device@3.3-impl \
     camera.device@3.4-impl \
+    camera.device@3.5-impl \
     libcameraclient_shim \
     Snap
 
