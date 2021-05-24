08aa03335347bbeb8ec75c5b958fdf8f1bd9252b
Deokgyu Yang <secugyu@gmail.com>
2021-05-25 00:59:00 +0900

    gts3l: Fix symbol error with libwvhidl
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I92264a5094ae6a5d05a589349f0007f21e01c02c

--- extract-files.sh
+++ extract-files.sh
@@ -55,19 +55,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib/libsample1.so)
-            sed -i 's|/data/misc/sample1|/data/misc/sample2|g' "${2}"
-            ;;
-        vendor/lib64/libsample2.so)
-            "${PATCHELF}" --remove-needed "libsample3.so" "${2}"
-            "${PATCHELF}" --add-needed "libsample4.so" "${2}"
-            ;;
-        vendor/lib/libsample5.so)
-            "${PATCHELF}" --replace-needed "libsample6.so" "libsample7.so" "${2}"
-            ;;
-        vendor/lib/libsample7.so)
-            "${PATCHELF}" --set-soname "libsample7.so" "${2}"
-            ;;
+    vendor/lib/libwvhidl.so)
+        "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v28.so" "${2}"
+        ;;
     esac
 }
 
