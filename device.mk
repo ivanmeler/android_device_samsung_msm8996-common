79c38449adb2fd743d3b4fb2b0fb9b781493cd6e
Deokgyu Yang <secugyu@gmail.com>
2021-05-26 01:42:19 +0900

    Revert "gts3l: audio: Add some packages"
    This reverts commit 032cb8a6871b5f1dfef37d96b72be0300f05f48f.

--- device.mk
+++ device.mk
@@ -87,11 +87,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0 \
-    android.hardware.audio.common@6.0 \
-    android.hardware.audio.common@6.0-util \
     android.hardware.audio@6.0-impl.gts3l:32 \
-    android.hardware.audio.effect@6.0 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl:32 \
