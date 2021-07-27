b988c9c984e003cd452c3156bb1fa7a8abf1d2b3
Deokgyu Yang <secugyu@gmail.com>
2021-07-27 22:12:42 +0900

    gts3l-common: audio: Vendor audio HAL working but no quad speakers
    This can be done bacause of removing all "Telephony ?X" things from
    audio_policy_configuration.xml. This should not be like this.
    
    Instead, now voice recording and headphone jack get working.
    
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I7c5a027d059ff5c69cd73bb2f129a1e5d07ad4aa

--- gts3l.mk
+++ gts3l.mk
@@ -106,13 +106,14 @@ PRODUCT_PACKAGES += \
     libaudioprimary_shim
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_platform_info_diff.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_diff.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
