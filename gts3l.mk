004299790168644e917cdef92848589cdcf597bd
Deokgyu Yang <secugyu@gmail.com>
2021-07-28 22:25:56 +0900

    gts3l-common: vibrator: Add vibrator supports to the common device tree
    Since the WiFi variants also supports vibrator as well as LTE version.
    
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I97e3333309986a2bda42bc09278ad9f46ad624da

--- gts3l.mk
+++ gts3l.mk
@@ -375,6 +375,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.gts3l
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
