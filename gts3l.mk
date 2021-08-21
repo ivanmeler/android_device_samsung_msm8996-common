820a0a3c0bf745801770bd6743b0b16e7f7206ea
Deokgyu Yang <secugyu@gmail.com>
2021-08-22 01:48:50 +0900

    gts3l-common: camera: Remove Snap camera since it doesn't work well
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: Ide6f4eaf049c81a649a75cf0b8e2c1a238ea3591

--- gts3l.mk
+++ gts3l.mk
@@ -132,7 +132,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     libshims_cameraclient \
