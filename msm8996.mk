8240c7238e80fd2f5be473a605beb7425b6f271b
Deokgyu Yang <secugyu@gmail.com>
2022-03-12 03:21:37 +0900

    gts3l-common: device: Build netd 1.0 too
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I65f76fe499dbc25d079fb757e3d92b6acd51eb47

--- msm8996.mk
+++ msm8996.mk
@@ -286,9 +286,11 @@ PRODUCT_COPY_FILES += \
 
 # Net
 PRODUCT_PACKAGES += \
+    android.system.net.netd@1.0 \
+    android.system.net.netd@1.0.vendor \
+    android.system.net.netd@1.1 \
     android.system.net.netd@1.1.vendor \
-    libandroid_net \
-    netutils-wrapper-1.0
+    libandroid_net
 
 # OMX
 PRODUCT_PACKAGES += \
