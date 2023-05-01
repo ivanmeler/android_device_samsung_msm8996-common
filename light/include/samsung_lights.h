39478c7bc174775f5c7923790963d8fcb9f2aab5
ivanmeler <i_ivan@windowslive.com>
2023-05-01 10:39:58 +0000

    Update overlays for s7

--- light/include/samsung_lights.h
+++ light/include/samsung_lights.h
@@ -29,9 +29,7 @@
 #define PANEL_MAX_BRIGHTNESS_NODE "/sys/class/leds/lcd-backlight/max_brightness"
 #define BUTTON_BRIGHTNESS_NODE "/sys/class/sec/sec_touchkey/brightness"
 
-// Those two paths don't exist in the Galaxy S7 QCOM sysfs
 #define LED_BLINK_NODE "/sys/class/sec/led/led_blink"
-#define LED_BLN_NODE "/sys/class/misc/backlightnotification/notification_led"
 
 // Uncomment to enable variable button brightness
 //#define VAR_BUTTON_BRIGHTNESS 1
