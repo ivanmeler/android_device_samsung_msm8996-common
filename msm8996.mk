fd353af1a2557e186f4197ef45924d7cdf84eae0
ivanmeler <i_ivan@windowslive.com>
2023-05-08 20:03:36 +0000

    Fix audio configs

--- msm8996.mk
+++ msm8996.mk
@@ -107,7 +107,6 @@ PRODUCT_PACKAGES += \
     libaudioprimary_shim
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
