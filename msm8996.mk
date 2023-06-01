f60d657fb885d708250426800b77e1b7a3c13e46
Ivan Meler <i_ivan@windowslive.com>
2023-06-01 13:26:35 +0200

    Update msm8996.mk

--- msm8996.mk
+++ msm8996.mk
@@ -288,7 +288,7 @@ PRODUCT_PACKAGES += \
     libandroid_net
 
 # Nfc
-PRODUCT_COPY_FILES
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
